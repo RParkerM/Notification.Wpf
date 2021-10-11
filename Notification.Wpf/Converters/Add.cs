8d7a8a652250b2aa1e9472ffd00d1a03b4259199
APlatonenkov <platonenkov87@gmail.com>
2021-10-11 12:14:20 +0300

    Corrects message height calculation when font style changes to non-standard

---
+++ Notification.Wpf/Converters/Add.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Notification.Wpf.Converters
+{
+    [ValueConversion(typeof(double), typeof(double)), MarkupExtensionReturnType(typeof(Add))]
+    internal class Add : ParameterMathConverter
+    {
+        public Add() : this(0) { }
+        public Add(double p) : base(p, (v, x) => v + x, (r, x) => r - x) { }
+    }
+}
---
+++ Notification.Wpf/Converters/ParameterMathConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Notification.Wpf.Converters
+{
+    [ValueConversion(typeof(double), typeof(double)), MarkupExtensionReturnType(typeof(ParameterMathConverter))]
+    internal abstract class ParameterMathConverter : DoubleValueConverter
+    {
+        private readonly Func<double, double, double> _To;
+        private readonly Func<double, double, double> _From;
+
+        public double Parameter { get; set; }
+
+        protected ParameterMathConverter(double Parameter, Func<double, double, double> to, Func<double, double, double> from = null)
+        {
+            this.Parameter = Parameter;
+            _To = to;
+            _From = from;
+        }
+
+        protected override double To(double v, object p) => _To(v, Parameter);
+
+        protected override double From(double v, object p) => (_From ?? throw new NotSupportedException()).Invoke(v, Parameter);
+    }
+}
